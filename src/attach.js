887a5b5be895baada71dd752dd2eec7053bb30b7
Aurélien LEMAITRE <chevlu@gmail.com>
2021-06-01 10:43:05 +0200

    Add safety call on request

--- src/attach.js
+++ src/attach.js
@@ -20,14 +20,14 @@ function interceptor(fetch, ...args) {
 
   // Register fetch call
   promise = promise.then(args => {
-    request = new Request(...args);
-    return fetch(request);
-  }).then(response => {
-    response.request = request;
-    return response;
-  }).catch(error => {
-    error.request = request;
-    return Promise.reject(error);
+    const request = new Request(...args);
+    return fetch(request).then(response => {
+      response.request = request;
+      return response;
+    }).catch(error => {
+      error.request = request;
+      return Promise.reject(error);
+    });
   });
 
   // Register response interceptors
